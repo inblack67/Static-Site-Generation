a7b8829e67f27294a85a22cb6e8f8707044a25a2
Aman <devproddeploy@gmail.com>
2020-09-03 19:43:27 +0530

    JSON.stringify

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,34 @@
 import fs from 'fs';
+import animes from '../src/data.json';
 
 const index = () => {
 
   return (
     <div>
-      home
+      <h1>Second arg of JSON.stringifiy</h1>
+      <div>
+        <h3>null</h3>
+        <pre>
+          {JSON.stringify(animes, null, 2)}
+        </pre>
+      </div>
+      <div>
+        <h3>Array - Filter result based on keys</h3>
+        <pre>
+          {JSON.stringify(animes, ['name'], 2)}
+        </pre>
+      </div>
+      <div>
+        <h3>function</h3>
+        <pre>
+          {JSON.stringify(animes, (key, value) => {
+            if(key.match('keyword')){
+              return '❌confidential❌';
+            }
+            return value;
+          }, 2)}
+        </pre>
+      </div>
     </div>
   )
 }
